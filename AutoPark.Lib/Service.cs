4490618d107dea3d5a4d68b0f2837e99e4c8cd5b
Блиновс <@223_15>
2018-05-16 20:21:38 +0600

    AutoPark

---
+++ AutoPark.Lib/Service.cs
@@ -0,0 +1,30 @@
+﻿using AutoPark.Lib.Modul;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPark.Lib
+{
+    public class Service
+    {
+        public Car Search(Project project, int GosNomer)
+        {
+            foreach (Car item in project.cars)
+            {
+                if (item.GosNomer == GosNomer) return item;
+            }
+            return null;
+        }
+
+        public Car Search(Project project, string CarModel)
+        {
+            foreach (Car item in project.cars)
+            {
+                if (item.CarModel == CarModel) return item;
+            }
+            return null;
+        }
+    }
+}
--- AutoPark/Program.cs
+++ AutoPark/Program.cs
@@ -1,4 +1,5 @@
-﻿using AutoPark.Lib.Generator;
+﻿using AutoPark.Lib;
+using AutoPark.Lib.Generator;
 using AutoPark.Lib.Modul;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,64 @@ namespace AutoPark
             {
                 item.PrintInfo();
             }
+
+            Console.WriteLine("Выберите проект: ");
+            foreach (var item in projects)
+                Console.WriteLine(" - " + item.ProjectName);
+
+            Project temp = null;
+            do
+            {
+                Console.Write("-> ");
+                string name = Console.ReadLine();
+
+                temp = projects.FirstOrDefault(o => o.ProjectName == name);
+
+                if (temp != null)
+                    break;
+
+                Console.WriteLine("Проект не найден!");
+            }
+            while (temp == null);
+
+            Console.WriteLine("Выберите критерии поиска: 1 - гаражный номер, 2 - модель");
+
+            int choise = 0;
+            do
+            {
+                Console.Write("-> ");
+                Int32.TryParse(Console.ReadLine(), out choise);
+
+            } while (choise != 1 && choise != 2);
+
+            int gosNomer = 0;
+            string modelCar;
+
+            Service service = new Service();
+            Car findCar = null;
+
+            switch (choise)
+            {
+                case 1:
+                    {
+                        Console.Write("Введите гос. номер машины: ");
+                        Int32.TryParse(Console.ReadLine(), out gosNomer);
+
+                        service.Search(temp, gosNomer);
+
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.Write("Введите модель машины: ");
+                        service.Search(temp, Console.ReadLine());
+                    }
+                    break;
+            }
+            if (findCar == null)
+                Console.WriteLine("Машина не найдена");
+            else
+                findCar.PrintInfo();
         }
     }
 }
